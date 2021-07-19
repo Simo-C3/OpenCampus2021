9d9d197f03c059aae861e891ccfcb0e842dc5c52
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-07-19 16:32:45 +0900

    [change] 画像参照のurlの変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
 
 <style>
 body {
-  background-image: url("https://simo-c3.github.io/image_url/C3-OpenCampus2021.png");
+  background-image: url("https://simo-c3.github.io/image_url/c3_opencampus2021.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
